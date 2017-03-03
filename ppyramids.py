bb8ebdc22a6e2af2edf1081abc8e2607d64b07b3
ivlch <ZX256@ZX256.local>
2017-03-03 21:00:41 +0100

    comments

--- ppyramids.py
+++ ppyramids.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-import sys
-
-text = input("What do you want to print?")
+#input word for the text
+text = input("What do you want to print? --> ")
 space = " "
 
-#width in characters
+#fixed width in characters
 width = 181
 r = int(width/len(text))
-repetition = int(input("How many repetitions?"))
+
+#number of pyramids
+repetition = int(input("How many repetitions? --> "))
 
 for i in range(repetition):
 	for i in range(r+1):
@@ -17,6 +18,3 @@ for i in range(repetition):
 	for i in range(r-1):
 		print(text*(r-i-1), end=" ")
 		print(space*i)
-
-#if __name__ == "__main__":
-    #main()
